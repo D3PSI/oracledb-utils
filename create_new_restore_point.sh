b9664151f26af14858fe2cb474212837ce62f6f0
Cedric Schwyter <cedric.schwyter@revendex.com>
2020-09-21 16:18:17 +0200

    Run restore point scripts as sysdba

--- create_new_restore_point.sh
+++ create_new_restore_point.sh
@@ -23,14 +23,13 @@ Usage: $0 -s SID -d HOST -t TENANT -n NAME [OPTIONS]
 
         -s, --service-id    :           The Oracle-SID to run the script against (Mandatory)
         -d, --db-host       :           The database host to run the script against (Defaults to localhost if not specified)
-        -t, --tenant        :           The tenant to run this script against (Mandatory)
         -n, --name          :           The name of the restore point (Mandatory)
 
     Examples:
 
-        $0 -s stgholp -t rockbank -n RESTORE_POINT_1
-        $0 --service-id stgholp --tenant rockbank --name RESTORE_POINT_1
-        $0 -s stgholp -t rockbank -n RESTORE_POINT_1 -d ordstg02.revendex.com
+        $0 -s stgholp -n RESTORE_POINT_1
+        $0 --service-id stgholp --name RESTORE_POINT_1
+        $0 -s stgholp -n RESTORE_POINT_1 -d ordstg02.revendex.com
 
 _EOF_
 }
@@ -39,7 +38,7 @@ _EOF_
 function main() {
     echo "Restoring complete database from local disk..."
     export ORACLE_SID=$arg_service_id
-    DB_SQLPLUS_START_SESSION="sqlplus $arg_tenant/$arg_tenant"
+    DB_SQLPLUS_START_SESSION="sqlplus / AS SYSDBA"
     DB_SQLPLUS_CREATE_CMD="CREATE RESTORE POINT $arg_name GUARANTEE FLASHBACK DATABASE;"
     if [ "$arg_db_host" != "localhost" ]; then
         echo "Database is not hosted on local machine, this functionality has not yet been implemented"
@@ -60,20 +59,18 @@ for arg in "$@"; do
     case "$arg" in
         "--service-id") set -- "$@" "-s";;
         "--db-host") set -- "$@" "-d";;
-        "--tenant") set -- "$@" "-t";;
         "--name") set -- "$@" "-n";;
         *) set -- "$@" "$arg"
     esac
 done
 
 OPTIND=1
-while getopts "hs:d:t:n:" opt
+while getopts "hs:d:n:" opt
 do
     case "$opt" in
         "h") usage; exit 0;;
         "s") arg_service_id=${OPTARG};;
         "d") arg_db_host=${OPTARG};;
-        "t") arg_tenant=${OPTARG};;
         "n") arg_name=${OPTARG};;
         "?") usage >&2; exit 1
     esac
@@ -89,11 +86,6 @@ fi
 if [[ -z "${arg_db_host}" ]]; then
     echo "No db-host specified, defaulting to 'localhost'..."
     arg_db_host="localhost"
-fi
-
-if [[ -z "${arg_tenant}" ]]; then
-    echo "No tenant specified, exiting..."
-    exit 1
 fi
 
 if [[ -z "${arg_name}" ]]; then
