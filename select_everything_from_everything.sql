34a7d50977241f8053b6ef42e4ca17658359cff8
Cedric Schwyter <cedric.schwyter@revendex.com>
2020-09-23 10:30:36 +0200

    Add to csv

--- select_everything_from_everything.sql
+++ select_everything_from_everything.sql
@@ -1,16 +1,29 @@
-SET serveroutput ON
-spool all_tables.txt
-BEGIN
-  FOR tab IN (SELECT * FROM all_tables WHERE owner='ROCKBANK')
-  LOOP
-    BEGIN
-      EXECUTE immediate 'SELECT * FROM ' || tab.table_name;
-        EXCEPTION
-          WHEN OTHERS THEN
-            dbms_output.put_line('Errors for table ' || tab.table_name || ': ' || SQLCODE);
-    END;
-  END LOOP;
-END;
+set serveroutput on;
+
+set lines 80 
+set head off 
+set colsep ','
+set pages 0
+set feed off
+
+spool runme.sql
+
+select 'set colsep '',''' from dual;
+select 'set lines 9999' from dual;
+select 'set head off' from dual;
+select 'set pages 0' from dual;
+select 'set feed off' from dual;
+
+begin
+    for table_rec in (select table_name from all_tables where owner='ROCKBANK') loop
+
+           dbms_output.put_line('spool '||table_rec.table_name||'.csv');
+           dbms_output.put_line('select * from '||table_rec.table_name||';');
+
+    end loop;
+end;
 /
+
+select ' spool off' from dual;
 spool off;
-SET serveroutput OFF
+set serveroutput off;
